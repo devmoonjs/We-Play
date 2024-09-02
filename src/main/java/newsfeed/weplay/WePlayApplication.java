8045f85a5604caf7915648a7e072560207e6167b
MOON <moonjs1018@naver.com>
2024-09-02 13:00:18 +0900

    first commit

---
+++ src/main/java/newsfeed/weplay/WePlayApplication.java
@@ -0,0 +1,13 @@
+package newsfeed.weplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WePlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WePlayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/newsfeed/weplay/WePlayApplicationTests.java
@@ -0,0 +1,13 @@
+package newsfeed.weplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WePlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
