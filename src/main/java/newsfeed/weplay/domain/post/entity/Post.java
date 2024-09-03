0412713af81d3b1db4d899e9b053f2347aba1a9f
Yehwan An <168810508+davidan94@users.noreply.github.com>
2024-09-03 15:06:01 +0900

    [FIX] : entity @Setter 삭제 및 다대일 관계 추가

--- src/main/java/newsfeed/weplay/domain/post/entity/Post.java
+++ src/main/java/newsfeed/weplay/domain/post/entity/Post.java
@@ -3,22 +3,22 @@ package newsfeed.weplay.domain.post.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import newsfeed.weplay.domain.user.entity.User;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "posts")
 @Getter
-@Setter
 @NoArgsConstructor
 public class Post {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 게시물 ID
 
-    @Column(nullable = false)
-    private Long userId; // 작성자 ID
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // 작성자 정보
 
     @Column(nullable = false)
     private String title; // 게시물 제목
@@ -44,5 +44,36 @@ public class Post {
     @PreUpdate
     public void preUpdate() {
         updatedAt = LocalDateTime.now();
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void update(Post updatedPost) {
+        this.title = updatedPost.getTitle();
+        this.content = updatedPost.getContent();
+        this.city = updatedPost.getCity();
+        this.updatedAt = LocalDateTime.now();
     }
 }
